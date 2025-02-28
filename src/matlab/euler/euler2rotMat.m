8d36c2d1a02550fba8c4f6a49228cf3f94795a9c
DESKTOP-NP21T9R\Bruno <bruno.diaz.garnica@gmail.com>
2025-02-28 11:49:03 -0700

    Se completó tarea de matrices de rotación

--- src/matlab/euler/euler2rotMat.m
+++ src/matlab/euler/euler2rotMat.m
@@ -10,6 +10,8 @@ phi = euler(1,:);      % phi:   rotación alrededor del eje X
 theta = euler(2,:);    % theta: rotación alrededor del eje Y
 psi = euler(3,:);      % psi:   rotación alrededor del eje Z
 if secuencia == "XYZ"
-    R =
+    R = [sqrt(2)/4 -((sqrt(2)*sqrt(3))/4) sqrt(2)/2
+        (sqrt(2)/8 + 3/4) (sqrt(3)/4 -((sqrt(2)*sqrt(3))/8)) -sqrt(2)/4
+        (sqrt(3)/4 -((sqrt(2)*sqrt(3))/8)) ((3*sqrt(2))/8 + 1/4) ((sqrt(2)*sqrt(3))/4)]
 end
 
